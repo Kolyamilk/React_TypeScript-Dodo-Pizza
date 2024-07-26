d394960d4f3c57e3721fb6b00eb6827e473f6138
Kolyamilk <Kolyamilk@mail.ru>
2024-07-26 13:20:57 +0300

    логика открывания и закрывания модального окна

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,11 +4,12 @@ interface ButtonProps {
     children: string;
     onClick: () => void;
     className?: string;
+    type?: any
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick, className, type }) => {
     return (
-        <button onClick={onClick} className={className}>{children}</button>
+        <button type={type} onClick={onClick} className={className}>{children}</button>
     );
 }
 
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,36 +5,44 @@ import Button from '../Button/Button';
 interface ButtonProps {
     children?: string;
     className?: string;
+    onClose: () => void
+  
+
 }
 
-const Modal: React.FC<ButtonProps> = ({ children, className }) => {
+const Modal: React.FC<ButtonProps> = ({ children, className, onClose }) => {
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault()
+    }
+
     return (
         <>
 
-            <div className='modal-fade' />
+            <div className='modal-fade' onClick={onClose} />
 
             <div className={className}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 25 25" className="sc-1cyl9gi-1 dJLpvD close"><path fill="#fff" fill-rule="evenodd" d="M9.846 12.499.553 3.205A1.878 1.878 0 0 1 3.208.55L12.5 9.843l9.291-9.29a1.878 1.878 0 0 1 2.655 2.655l-9.29 9.29 9.293 9.294a1.878 1.878 0 0 1-2.655 2.655L12.5 15.154 3.205 24.45A1.878 1.878 0 0 1 .55 21.795z" clip-rule="evenodd"></path></svg>
-                <div className="sm">
-                    <h3 className='title'>{children}</h3>
-                    <p className='description'>Подарим подарок на день рождения, сохраним адрес доставки и расскажем об акциях</p>
-                </div>
-                <div className="lg">
-                    <label>
-                        <span className="label">Номер телефона</span>
-                        <div className="input-container">
-                            <input data-testid="checkout-form__phone-input" type="tel" placeholder="+7 999 999-99-99" id="phn-input" data-error="false" data-validation="false" data-clear="false" value="+7 123 333-33-33">
-                            </input>
-                        </div>
-                    </label>
-                </div>
-                <div className="sm">
-                    <Button className='big' onClick={() => { }}>Выслать код</Button>
-                    <div className="fz-12">Продолжая, вы соглашаетесь
-                        <a href="/moscow/legal " className='legal' target="_blank"> со сбором и обработкой персональных данных и пользовательским соглашением</a>
+                <form onSubmit={submitHandler}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 25 25" className="close"><path fill="#fff" fill-rule="evenodd" d="M9.846 12.499.553 3.205A1.878 1.878 0 0 1 3.208.55L12.5 9.843l9.291-9.29a1.878 1.878 0 0 1 2.655 2.655l-9.29 9.29 9.293 9.294a1.878 1.878 0 0 1-2.655 2.655L12.5 15.154 3.205 24.45A1.878 1.878 0 0 1 .55 21.795z" clip-rule="evenodd"></path></svg>
+                    <div className="sm">
+                        <h3 className='title'>{children}</h3>
+                        <p className='description'>Подарим подарок на день рождения, сохраним адрес доставки и расскажем об акциях</p>
                     </div>
-                </div>
-
+                    <div className="lg">
+                        <label>
+                            <span className="label">Номер телефона</span>
+                            <div className="input-container">
+                                <input data-testid="checkout-form__phone-input" type="tel" placeholder="+7 999 999-99-99" id="phn-input" data-error="false" data-validation="false" data-clear="false" value="+7 123 333-33-33">
+                                </input>
+                            </div>
+                        </label>
+                    </div>
+                    <div className="sm">
+                        <Button type='submit' className='big' onClick={() => { }}>Выслать код</Button>
+                        <div className="fz-12">Продолжая, вы соглашаетесь
+                            <a href="/moscow/legal " className='legal' target="_blank"> со сбором и обработкой персональных данных и пользовательским соглашением</a>
+                        </div>
+                    </div>
+                </form>
             </div>
         </>
     )
